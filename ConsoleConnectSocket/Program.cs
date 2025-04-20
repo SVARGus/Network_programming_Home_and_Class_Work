0abd9f8fc1f2ffddeba5039c74973cfe378ab449
SVARGus <kyznetsovpn@mail.ru>
2025-04-20 21:12:34 +0300

    Практическое задание №1 Реализован клиентская часть консольного приложения

---
+++ ConsoleConnectSocket/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using static System.Console;
+
+/*
+Практическое задание (на уроке не успели реализовать), ушло как домашнее задание
+Задание 1
+	Разработайте два консольных приложения, использующих сокеты. Одно приложение – сервер, второе - клиент. 
+    Клиентское приложение посылает приветствие серверу. Сервер отвечает. И клиент, и сервер отображают полученное сообщение. 
+    Пример вывода:
+	Сервер: В 10:25 от [IP-адрес] получена строка: Привет, сервер!
+	Клиент: В 10:26 от [IP-адрес] получена строка: Привет, клиент! 
+    Используйте механизм синхронных сокетов
+
+Реализация клиентской части
+*/
+
+namespace ConsoleConnectSocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 1024;
+            string serverIP = "127.0.0.1";
+            IPAddress ip = IPAddress.Parse(serverIP);
+            IPEndPoint ep = new IPEndPoint(ip, port);
+            Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+            try
+            {
+                sender.Connect(ep);
+                if (sender.Connected)
+                {
+                    string message = "Привет, Сервер!";
+                    sender.Send(Encoding.ASCII.GetBytes(message));
+
+                    byte[] buffer = new byte[4096];
+                    int bytesRec = sender.Receive(buffer);
+                    string data = Encoding.ASCII.GetString(buffer, 0, bytesRec);
+
+                    string serverIPAddress = ((IPEndPoint)sender.RemoteEndPoint).Address.ToString();
+                    WriteLine($"Клиент: в {DateTime.Now:HH:mm} от {serverIPAddress} получена строка: {data}");
+                }
+                else
+                {
+                    WriteLine("Error");
+                }
+            }
+            catch (SocketException ex)
+            {
+                WriteLine(ex.Message);
+            }
+            finally
+            {
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+        }
+    }
+}
